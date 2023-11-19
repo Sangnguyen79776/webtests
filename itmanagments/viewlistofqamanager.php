d6398b9a14c864cee803a58a4ca58cdc1c1b54bc
Sangnguyen79776 <145439978+Sangnguyen79776@users.noreply.github.com>
2023-11-19 10:33:22 -0600

    Update viewlistofqamanager.php

--- itmanagments/viewlistofqamanager.php
+++ itmanagments/viewlistofqamanager.php
@@ -9,55 +9,95 @@ $stmt = $con->prepare("SELECT username,email,password,role FROM acc WHERE id = ?
 // In this case we can use the account ID to get the account info.
 $stmt->bind_param('i', $_SESSION['id']);
 $stmt->execute();
-$stmt->bind_result($username,$email, $password,$role);
+$stmt->bind_result($username, $email, $password, $role);
 $stmt->fetch();
 $stmt->close();
 ?>
 <html>
-    <head>
-       
-    </head>
+
+<head>
+    <style>
+    h1 {
+        font-size: 50px;
+    }
+
+    img {
+        position: absolute;
+        left: 0px;
+        top: 0px;
+        z-index: -1;
+        border-radius: 50%;
+        width: 100%;
+        opacity: 0.7;
+    }
+
+    table {
+        border-collapse: collapse;
+        width: 100%;
+    }
+
+    th,
+    td {
+        padding: 8px;
+        text-align: left;
+        border-bottom: 1px solid #ddd;
+    }
+
+    th,
+    #a,
+    a {
+        text-align: center;
+    }
+    </style>
+</head>
 
 <div>
-<a href="adminhomepage.php">Admin homepage </a>
-			
-			
-		<h1 > List of QA manager details are below: </h1></div>
-				<table width="100%" border="1" style="border-collapse:collapse;">
-                <thead>
-<tr>
-<th ><strong>S.No</strong></th>
-<th><strong>QaManger username</strong></th>
-<th><strong>QaManager email</strong></th>
-<th><strong>QaManager password </strong></th>
+    <a href="adminhomepage.php">Admin homepage </a>
+    <img src="picture4.png">
 
-<th><strong>Role of QaManager</strong></th>
+    <h1> List of QA manager details are below: </h1>
+</div>
+<table width="100%" border="1" style="border-collapse:collapse;">
+    <thead>
+        <tr>
+            <th><strong>S.No</strong></th>
+            <th><strong>QaManger username</strong></th>
+            <th><strong>QaManager email</strong></th>
+            <th><strong>QaManager password </strong></th>
 
+            <th><strong>Role of QaManager</strong></th>
+        </tr>
+    </thead>
+    <tbody>
+        <?php
+        $count = 1;
+        $sel_query = "SELECT * FROM acc WHERE role ='QAmanager'ORDER BY id desc ";
+        $result = mysqli_query($con, $sel_query);
+        while ($row = mysqli_fetch_assoc($result)) { ?>
+        <tr>
+            <td id="a">
+                <?php echo $count; ?>
+            </td>
+            <td id="a">
+                <?php echo $row["username"]; ?>
+            </td>
+            <td id="a">
+                <?php echo $row["email"]; ?>
+            </td>
+            <td id="a">
+                <?php echo $row["password"]; ?>
+            </td>
+            <td id="a">
+                <?php echo $row["role"]; ?>
+            </td>
+            <td><a href="editqamanageracc.php?id=<?php echo $row["id"]; ?>">Edit</a></td>
+            <td><a href="deleteqamanageracc.php?id=<?php echo $row["id"]; ?>">Delete</a></td>
+        </tr>
+        <?php $count++;
+        } ?>
+    </tbody>
 
+    </div>
+    </body>
 
-</tr>
-                </thead>
-<tbody>
-<?php
-$count=1;
-$sel_query="SELECT * FROM acc WHERE role ='QAmanager'ORDER BY id desc ";
-$result = mysqli_query($con,$sel_query);
-while($row = mysqli_fetch_assoc($result)) { ?>
-<tr><td ><?php echo $count; ?></td>
-<td ><?php echo $row["username"]; ?></td>
-<td ><?php echo $row["email"]; ?></td>
-<td ><?php echo $row["password"]; ?></td>
-
-<td ><?php echo $row["role"];?></td>
-<td><a href="editqamanageracc.php?id=<?php echo $row["id"]; ?>">Edit</a></td>
-<td><a href="deleteqamanageracc.php?id=<?php echo $row["id"]; ?>">Delete</a></td>
-
-
-
-</tr>
-<?php $count++; } ?>
-</tbody>
-
-			</div>
-</body>
 </html>
